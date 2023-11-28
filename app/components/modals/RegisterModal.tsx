e862eb89d7d869213f509e288c1b0c86c0b099be
Elham Mirzavand <elham.mirzavand@gmail.com>
2023-11-28 18:45:45 +0100

    Finish UI for register

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -6,6 +6,10 @@ import { useCallback, useState } from "react"
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
 import useRegisterModal from '@/app/hooks/useRegisterModal'
 import Modal from './Modal'
+import Heading from '../Heading'
+import Input from '../inputs/Input'
+import toast from 'react-hot-toast/headless'
+import Button from '../Button'
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -27,26 +31,56 @@ const RegisterModal = () => {
 
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
-    
+
     axios.post('/api/register', data)
       .then(() => {
         registerModal.onClose();
       })
       .catch((error) => {
-        console.log(error);
+        toast.error("Something ")
       })
       .finally(() => {
         setIsLoading(false);
       })
   }
+
+  const bodyContent = (
+    <div className='flex flex-col gap-4'>
+      <Heading
+        title='Welcome to Airbnb'
+        subTitle='Create an account!'
+      />
+      <Input id='email' label='Email' disabled={isLoading} register={register} errors={errors} required />
+      <Input id='name' label='Name' type='password' disabled={isLoading} register={register} errors={errors} required />
+      <Input id='password' label='Password' type='password' disabled={isLoading} register={register} errors={errors} required />
+    </div>
+  )
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button outline label='Continue with Google' icon={FcGoogle} onClick={()=>{}}/>
+      <Button outline label='Continue with Github' icon={AiFillGithub} onClick={()=>{}}/>
+      <div className="text-neutral-500 text-center m-4 font-light">
+        <div className="justify-center flex flex-row items-center gap-2">
+          <div>Already have an account?</div>
+          <div onClick={registerModal.onClose} className="text-neutral-800 cursor-pointer hover:underline">
+            Log in
+          </div>
+        </div>
+      </div>
+    </div>
+  )
   return (
-    <Modal 
+    <Modal
       disabled={isLoading}
       isOpen={registerModal.isOpen}
       title='Register'
       actionLabel='Continue'
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
     />
   )
 }
